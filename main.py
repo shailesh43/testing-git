0cc54bd77573f36a0777fe1303af032df0b3b755
shailesh43 <sathehshailesh43@gmail.com>
2025-01-25 20:59:25 +0530

    implement basic arithmetics functions

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hey folks, welcome to git and github tutorial")
+
+# Basic arithmetics
+def power(a, b):
+    return a**b
+
+def remainder(n, x):
+    return n%x
+
+
+print(power(2,3))
+print(remainder(50, 3))
